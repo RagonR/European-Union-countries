f837cc58e7e66119334c09a6b49e775720388e9f
dragonlais <rapolasrusakevicius@gmail.com>
2021-12-02 13:23:37 +0200

    Initial commit

---
+++ src/main/java/com/example/European/Union/countries/EuropeanUnionCountriesTop10Application.java
@@ -0,0 +1,13 @@
+package com.example.European.Union.countries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuropeanUnionCountriesTop10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuropeanUnionCountriesTop10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/European/Union/countries/EuropeanUnionCountriesTop10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.European.Union.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuropeanUnionCountriesTop10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
